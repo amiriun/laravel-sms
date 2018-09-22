02852d5d608f4c80a98c1b2d29f715793a169412
Amir Alian <amircup2006@gmail.com>
2018-09-22 11:56:38 +0330

    Add gateway mapper to config

--- src/config/sms.php
+++ src/config/sms.php
@@ -1,5 +1,18 @@
 <?php
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default SMS Gateway
+    |--------------------------------------------------------------------------
+    |
+    | When your application is in debug mode, sms messages should not be send to endusers.
+    | for handle this issue you can use "debug" gateway to log messages instead of send to users.
+    | supported: debug , kavenegar
+    |
+    */
+    'default_gateway' => 'debug',
+
     'kavenegar' => [
         'api_key' => '',
         'numbers' => [
@@ -17,5 +30,10 @@ return [
             '20003002',
             '20003003',
         ],
+    ],
+
+    'map_gateway_to_connector' => [
+        'debug' => \Amiriun\SMS\Services\Connectors\DebugConnector::class,
+        'kavenegar' => \Amiriun\SMS\Services\Connectors\KavenegarConnector::class,
     ],
 ];
